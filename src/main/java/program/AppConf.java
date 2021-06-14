19ba430252b730466659f0db5d4c16e54a8ae867
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-06-14 22:23:29 +1000

    Не вызывается @Configuration и не видит application.properties

---
+++ src/main/java/program/AppConf.java
@@ -0,0 +1,7 @@
+package program;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConf {
+}
--- src/main/java/program/Program.java
+++ src/main/java/program/Program.java
@@ -1,5 +1,6 @@
 package program;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
