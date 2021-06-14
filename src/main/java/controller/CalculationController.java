ed8c6b5ea215d741eb2d553d4e4c4b6394971822
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-06-14 20:35:43 +1000

    Spring init + legacy removed

--- src/main/java/controller/CalculationController.java
+++ src/main/java/controller/CalculationController.java
@@ -1,41 +1,51 @@
-package demo;
+package controller;
 
 import model.DataValidation;
 import model.ToArrayParser;
 import model.calculator.AbstractCalculation;
 import model.calculator.SumCalculator;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
-@SpringBootApplication
-public class Program {
-    public static void main(String[] args) throws Exception {
+public class CalculationController {
+
+    public CalculationController() {
+        start();
+    }
+
+    public void start() {
         String input = input();
         int[] data = ToArrayParser.parseFromString(input);
 
         output(new SumCalculator(data));
     }
 
-    private static void output(AbstractCalculation calculation) {
+    private void output(AbstractCalculation calculation) {
         System.out.printf("Result of %s operation with %s is %d",
                           calculation.getOperationName(),
                           calculation.getSource(),
                           calculation.execute());
     }
 
-    private static String input() throws Exception {
+    private String input() {
         System.out.println("Enter number:");
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
 
-        String input = reader.readLine();
-        while (DataValidation.validate(input) == false) {
-            System.out.println("Invalid input\nTry again!");
+        String input = "";
+        try {
             input = reader.readLine();
+            while (DataValidation.validate(input) == false) {
+                System.out.println("Invalid input\nTry again!");
+                input = reader.readLine();
+            }
+        } catch (IOException exception) {
+            System.out.println("Input error:\n" + exception.getMessage());
         }
 
         return input;
     }
+
 }
---
+++ src/test/java/testing/CalculationSpringBootTest.java
@@ -0,0 +1,14 @@
+package testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import program.Program;
+
+@SpringBootTest(classes = Program.class)
+public class CalculationSpringBootTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
