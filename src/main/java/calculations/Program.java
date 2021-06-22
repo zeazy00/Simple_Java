1d9b0fab35d470a21ba07d9332b80e23cd0d7d12
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-06-22 21:30:42 +1000

    anyway it doesn't work!

--- src/main/java/calculations/Program.java
+++ src/main/java/calculations/Program.java
@@ -1,19 +1,32 @@
 package calculations;
 
 import calculations.controller.CalculationController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Program {
+@ConditionalOnProperty(value = "test", havingValue = "false")
+public class Program implements CommandLineRunner {
+
+    @Autowired
+    CalculationController calculationController;
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(Program.class, args);
-        context.getBean(CalculationController.class).start();
+        SpringApplication.run(Program.class, args);
+//        ApplicationContext context = SpringApplication.run(Program.class, args);
+//        context.getBean(CalculationController.class).start();
         //не стал использовать implements CommandLineRunner и
         //вызов CalculationController.start() в  @Override run
         //потому что тогда на тестах этот метод тоже вызывается
         //а этого мне не надо
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        calculationController.start();
     }
 }
