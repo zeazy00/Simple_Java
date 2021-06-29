f27d2cf718d25b3087da98b0edf3eea133cab0c4
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-06-29 13:06:23 +1000

    exception handler

---
+++ src/main/java/calculations/exceptionhandler/RestExceptionHandler.java
@@ -0,0 +1,28 @@
+package calculations.exceptionhandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.naming.OperationNotSupportedException;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(OperationNotSupportedException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String operationNotFoundHandler(OperationNotSupportedException ex) {
+        return "Can't execute operation " + ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String exceptionHandler(Exception ex) {
+        return ex.getMessage();
+    }
+
+}
