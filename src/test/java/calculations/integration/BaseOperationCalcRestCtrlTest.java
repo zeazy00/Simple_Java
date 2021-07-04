68208358f3a2ee1a70c7a75ac6d362227751f7dd
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-07-04 14:00:52 +1000

    test edition base class for all operations extracted

--- src/test/java/calculations/integration/BaseOperationCalcRestCtrlTest.java
+++ src/test/java/calculations/integration/BaseOperationCalcRestCtrlTest.java
@@ -1,43 +1,51 @@
 package calculations.integration;
 
-import calculations.controller.dto.OperationResultDTO;
-import com.google.gson.Gson;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class PostBodySumCalcRestCtrlTest extends BaseOperationCalcRestCtrlTest {
+public class BaseOperationCalcRestCtrlTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
 
     @Test
-    void sumValidTest() throws Exception {
-
+    void emptyInputTest() throws Exception{
         //Arrange
-        String bodyJson = "1454668";
-        Gson gson = new Gson();
+        String bodyJson = "";
 
         //act
         ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/math/calculate/sum")
                                                                             .contentType(MediaType.APPLICATION_JSON)
                                                                             .content(bodyJson));
+
         //assert
-        MvcResult result = resultActions.andExpect(
+        resultActions.andExpect(
                 MockMvcResultMatchers.status()
-                                     .isOk()).andReturn();
+                                     .isBadRequest());
+    }
 
-        String jsonString = result.getResponse().getContentAsString();
-        OperationResultDTO resultDTO = gson.fromJson(jsonString, OperationResultDTO.class);
+    @Test
+    void negativeNumberInputTest() throws Exception{
+        //Arrange
+        String bodyJson = "-4449";
 
-        Assertions.assertEquals(resultDTO.getOperationName(),"Sum");
-        Assertions.assertEquals(resultDTO.getResult(),34);
+        //act
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/math/calculate/sum")
+                                                                            .contentType(MediaType.APPLICATION_JSON)
+                                                                            .content(bodyJson));
+
+        //assert
+        resultActions.andExpect(
+                MockMvcResultMatchers.status()
+                                     .isBadRequest());
     }
 }
