97262863332cb9e10c26f9cb7b31d1480a00629e
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-07-04 14:01:35 +1000

    all command executor interface

---
+++ src/main/java/calculations/actions/allexecutor/imp/AllCommandExecutorAction.java
@@ -0,0 +1,16 @@
+package calculations.actions.allexecutor.imp;
+
+import calculations.actions.allexecutor.AllCommandExecutor;
+import calculations.controller.dto.OperationResultDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AllCommandExecutorAction implements AllCommandExecutor {
+
+    @Override
+    public List<OperationResultDTO> executeAll(int input) {
+        return null;
+    }
+}
