08bfcb506e529745a004a21ad58a2e47e62bfff1
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-08-19 15:36:28 +1000

    querydsl filtration

--- src/main/java/calculations/model/repository/MathExpressionRepository.java
+++ src/main/java/calculations/model/repository/MathExpressionRepository.java
@@ -3,11 +3,13 @@ package calculations.model.repository;
 import calculations.model.entity.MathExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface MathExpressionRepository extends JpaRepository<MathExpression, UUID>,
-                                                  JpaSpecificationExecutor<MathExpression> {
+                                                  JpaSpecificationExecutor<MathExpression>,
+                                                  QuerydslPredicateExecutor<MathExpression> {
     List<MathExpression> findByInput(String input);
 }
